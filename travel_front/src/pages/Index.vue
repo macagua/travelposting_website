35b358a60966cf3a53407f406fc34f90e2942560
chinuxparibus <grilletjhony@gmail.com>
2019-09-02 17:20:30 -0400

    Agrega componentes base para landing y visualización

---
+++ travel_front/src/pages/Index.vue
@@ -0,0 +1,54 @@
+<template>
+  <q-page>
+    <section class="home text-center text-white flex flex-center column window-width window-height">
+      <h1 class="">Travel & Solutions</h1>
+      <h2>Siempre avanzando hacia su éxito</h2>
+    </section>
+    <section class="testimony flex window-width window-height q-py-xl text-body1">
+      <div class="row">
+        <div class="col-xs-8 offset-xs-2">
+          <div class="row q-gutter-xl">
+            <div class="col">
+              <div class="text-center">
+                <q-icon size="xl" name="account_balance" />
+              </div>
+              <h3 class="text-center"> Tradición </h3>
+              <p class="text-justify"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+              do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+            <div class="col">
+              <div class="text-center">
+                <q-icon size="xl" name="language" />
+              </div>
+              <h3 class="text-center"> Internacional </h3>
+              <p class="text-justify"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+            </div>
+            <div class="col">
+              <div class="text-center">
+                <q-icon size="xl" name="thumb_up" />
+              </div>
+              <h3 class="text-center"> Confiable </h3>
+              <p class="text-justify"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
+
+<style lang="stylus" scoped>
+.home
+  background linear-gradient(135deg, rgba(9,9,121,1) 30%, $primary 100%)
+</style>
