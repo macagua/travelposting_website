b66073eec7198539b6346b451b81e11dd3c455a2
Dehivis Perez <dehivis.oliver@gmail.com>
2019-09-26 22:43:48 -0400

    new destination urls

--- travel/apps/destinations/urls.py
+++ travel/apps/destinations/urls.py
@@ -1,1 +1,56 @@
+from django.urls import path
 
+from apps.destinations.views import (
+    DestinationListView,
+    DestinationCreateView,
+    DestinationDetailView,
+    DestinationUpdateView,
+    DestinationDeleteView,
+    GalleryListView,
+    OptionTabDataTemplateAjaxView,
+)
+
+app_name = 'destinations'
+urlpatterns = [
+    path(
+        '',
+        DestinationListView.as_view(),
+        name='list',
+    ),
+
+    path(
+        'create/',
+        DestinationCreateView.as_view(),
+        name='create',
+    ),
+
+    path(
+        '<int:pk>/create/',
+        DestinationDetailView.as_view(),
+        name='detail',
+    ),
+
+    path(
+        '<int:pk>/update/',
+        DestinationUpdateView.as_view(),
+        name='update',
+    ),
+
+    path(
+        '<int:pk>/delete/',
+        DestinationDeleteView.as_view(),
+        name='delete',
+    ),
+
+    path(
+        '<int:pk>/gallery/',
+        GalleryListView.as_view(),
+        name='gallery-list',
+    ),
+
+    path(
+        'option/<int:pk>/template/',
+        OptionTabDataTemplateAjaxView.as_view(),
+        name='option-template',
+    ),
+]
