c50aa868b38f1e6e8201bf4ad93f9ae36ff4560f
Eliezer Romero <eliezerfot123@gmail.com>
2019-09-27 14:03:39 -0400

    modelstranlations for accounts and destinations

---
+++ travel/apps/destinations/translation.py
@@ -0,0 +1,32 @@
+from modeltranslation.translator import (
+    register,
+    TranslationOptions,
+)
+
+from apps.destinations.models import (
+    Categorie,
+    Destination,
+    Photo,
+    Badge,
+    OptionTabData,
+)
+
+@register(Categorie)
+class CategorieTranslationOptions(TranslationOptions):
+    fields = ('short_description','status',)
+
+@register(Destination)
+class DestinationTranslationOptions(TranslationOptions):
+    fields = ('name','short_description','description',)
+
+@register(Photo)
+class PhotoTranslationOptions(TranslationOptions):
+    fields = ('name','description',)
+
+@register(Badge)
+class BadgeTranslationOptions(TranslationOptions):
+    fields = ('name','description',)    
+
+@register(OptionTabData)
+class OptionTabDataTranslationOptions(TranslationOptions):
+    fields = ('name','description','template',)       
