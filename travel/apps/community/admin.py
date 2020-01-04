7178f287adb75a0d9198835dc51a97448bffdef2
Eliezer Romero <eliezerfot123@gmail.com>
2020-01-04 08:51:36 -0400

    Modelo para el mensajeria interna

--- travel/apps/community/admin.py
+++ travel/apps/community/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
-from apps.community.models import Recommendation
+from apps.community.models import Recommendation, Message
 # Register your models here.
 
+
 admin.site.register(Recommendation)
+
+
+class MessageAdmin(admin.ModelAdmin):
+    model = Message
+    list_display = ('id', 'sender', 'content', )
+
+admin.site.register(Message, MessageAdmin)
--- travel/apps/community/models.py
+++ travel/apps/community/models.py
@@ -1,8 +1,11 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 from apps.destinations.models import Destination
 from django.contrib.auth.models import User
 from apps.accounts.models import CustomerUser
 from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
+from django.core.exceptions import ValidationError
 
 
 # Create your models here.
@@ -30,3 +33,32 @@ class Recommendation(models.Model):
 
     def __str__(self):
         return self.destino
+
+
+#A private directmessage
+class Message(models.Model):
+    content = models.TextField(_('Content'))
+    sender = models.ForeignKey(
+        CustomerUser, related_name='sent_dm', verbose_name=_("Sender"), on_delete=models.CASCADE)
+    recipient = models.ForeignKey(
+        CustomerUser, related_name='received_dm', verbose_name=_("Recipient"), on_delete=models.CASCADE)
+    sent_at = models.DateTimeField(_("sent at"), null=True, blank=True)
+    read_at = models.DateTimeField(_("read at"), null=True, blank=True)
+
+    @property
+    def unread(self):
+        """returns whether the message was read or not"""
+        if self.read_at is not None:
+            return False
+        return True
+
+    def __str__(self):
+        return self.content
+
+    def save(self, **kwargs):
+        if self.sender == self.recipient:
+            raise ValidationError(_("You can't send messages to yourself"))
+
+        if not self.id:
+            self.sent_at = timezone.now()
+        super(Message, self).save(**kwargs)
