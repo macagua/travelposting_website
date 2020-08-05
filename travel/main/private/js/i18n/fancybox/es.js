737cca3495540a7928565dcd6814d42aa9d32814
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2020-08-05 01:57:34 -0400

    Agregar soporte a traduccion al espanol de la libreria jquery.fancybox #140

---
+++ travel/main/private/js/i18n/fancybox/es.js
@@ -0,0 +1,17 @@
+// Taken from https://github.com/fancyapps/fancybox/issues/1753
+
+$.fancybox.defaults.i18n.es = {
+    CLOSE       : 'Cerrar',
+    NEXT        : 'Siguiente',
+    PREV        : 'Anterior',
+    ERROR       : 'No se puede cargar el contenido solicitado. <br/> Por favor, intente de nuevo.',
+    PLAY_START  : 'Inicias diapositivas',
+    PLAY_STOP   : 'Pausar diapositivas',
+    FULL_SCREEN : 'Pantalla completa',
+    THUMBS      : 'Miniaturas',
+    DOWNLOAD    : 'Descargar',
+    SHARE       : 'Compartir',
+    ZOOM        : 'Ampliar'
+};
+
+$.fancybox.defaults.lang = 'es';
