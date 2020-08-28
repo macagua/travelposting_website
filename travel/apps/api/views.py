dcae4aebc86efc6eb55db0a92e06ab6c456ba9ac
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2020-08-28 16:40:36 -0400

    Added Video Gallery loader for a Destination

--- travel/apps/api/views.py
+++ travel/apps/api/views.py
@@ -10,7 +10,7 @@ from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from apps.api import serializers
-from apps.destinations.models import Photo, Destination
+from apps.destinations.models import Photo, Video, Destination
 
 
 logger = logging.getLogger(__name__)
@@ -20,6 +20,7 @@ class DestinationViewSet(ModelViewSet):
     serializer_class = serializers.DestinationSerializer
     queryset = Destination.objects.all()
     serializer_photo_class = serializers.PhotoWithDestinationSerializer
+    serializer_video_class = serializers.VideoWithDestinationSerializer
     pagination_class = pagination.PageNumberPagination
     filter_backends = (filters.OrderingFilter, filters.SearchFilter, DjangoFilterBackend)
     search_fields = ('user__username', 'name', 'description')
@@ -92,6 +93,81 @@ class DestinationViewSet(ModelViewSet):
         try:
             instance = Photo.objects.get(pk=id)
         except Photo.DoesNotExist:
+            raise Http404()
+
+        self.perform_destroy(instance)
+        data = {
+            'status': True,
+            'statusText': 'success'
+        }
+        return Response(data, status=status.HTTP_200_OK)
+
+    # Video Gallery for a Destination
+    @action(detail=True, methods=['get'], permission_classes=[AllowAny])
+    def video(self, request, pk=None):
+        obj = self.get_object()
+        queryset = obj.video.all()
+        serializer = self.serializer_video_class(queryset, many=True)
+        return Response(serializer.data)
+
+    @action(detail=True, methods=['post'], url_path="video/create", permission_classes=[AllowAny])
+    def video_create(self, request, pk=None):
+        data = request.data.copy()
+        data['destination'] = pk
+        data.setdefault('sort', 999)
+        name = f"{data['video'].name}.{data['video'].content_type.split('/')[1]}"
+        data['video'].name = name
+        data.setdefault('name', name.split('.')[0])
+        data.setdefault('thumbnail', get_thumbnailer(
+            data['video'],
+            f"{name.split('.')[0]}_thumbnail.{data['video'].content_type.split('/')[1]}"))
+        serializer = self.serializer_video_class(data=data)
+
+        if serializer.is_valid():
+            self.perform_create(serializer)
+
+            results = {
+                'status': True,
+                'statusText': 'success',
+                'model': serializer.data
+            }
+            status_code = status.HTTP_201_CREATED
+        else:
+            results = {
+                'status': False,
+                'statusText': ', '.join([f"{k}: {v}" for k, v in serializer.errors.items()])
+            }
+            status_code = status.HTTP_200_OK
+        headers = self.get_success_headers(serializer.data)
+        return Response(results, status=status_code, headers=headers)
+
+    @action(detail=True, methods=['post', 'put', 'patch'], url_path="video/update", permission_classes=[AllowAny])
+    def video_update(self, request, pk=None):
+        id = request.POST.get('id')
+
+        try:
+            instance = Video.objects.get(pk=id)
+        except Video.DoesNotExist:
+            raise Http404()
+
+        serializer = self.serializer_video_class(instance, data=request.data, partial=True)
+
+        data = {
+            'status': serializer.is_valid(),
+            'statusText': ','.join(
+                [f"{k}: {v}" for k, v in serializer.errors.items()]) if serializer.errors else 'success',
+        }
+        self.perform_update(serializer)
+        data.update({'model': serializer.data})
+        return Response(data, status=status.HTTP_200_OK)
+
+    @action(detail=True, methods=['post', 'delete'], url_path='video/delete', permission_classes=[AllowAny])
+    def video_delete(self, request, pk=None):
+        id = request.POST.get('id')
+
+        try:
+            instance = Video.objects.get(pk=id)
+        except Video.DoesNotExist:
             raise Http404()
 
         self.perform_destroy(instance)
--- travel/apps/destinations/views/destination.py
+++ travel/apps/destinations/views/destination.py
@@ -330,6 +330,23 @@ class GalleryListView(NoCommunityRequiredMixin, SingleObjectMixin, ListView):
     def get_queryset(self):
         return self.object.gallery.all()
 
+
+class VideoListView(NoCommunityRequiredMixin, SingleObjectMixin, ListView):
+    template_name = 'destinations/gallery/_list_video.html'
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object(queryset=Destination.objects.all())
+        return super(VideoListView, self).get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(VideoListView, self).get_context_data(**kwargs)
+        context['destination'] = self.object
+        return context
+
+    def get_queryset(self):
+        return self.object.media.all()
+
+
 class BaseItineraryMixin(object):
     form_class = ItineraryForm
     template_name = 'destinations/itinerary/itinerary_create.html'
